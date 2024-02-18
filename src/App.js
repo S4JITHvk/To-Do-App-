96af41ba7b0586199bee69495f419e5e91f98ddb
S4JITHvk <sajithvk04@gmail.com>
2024-02-18 12:58:51 +0530

    Validation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+// import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [todolist, setTodolist] = useState([]);
+  const [newTask, setNewTask  ] = useState("");
+
+  const handleChange = (e) => {
+    setNewTask(e.target.value);
+  };
+
+  const addNewTask = () => {
+    console.log("hello")
+    if (newTask.trim() !== "") {
+      setTodolist([...todolist, newTask.trim()]);
+      setNewTask("");
+      console.log("Todo=>>",todolist)
+    }
+  };
+  
+
+  const deleteTask = (taskName) => {
+    const newTodoList = todolist.filter((task) => task !== taskName);
+    setTodolist(newTodoList);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <header>Todo App</header>
+      <div className="inputField">
+        <input
+          type="text"
+          placeholder="Add your new todo"
+          value={newTask}
+          onChange={handleChange}
+        />
+        <button onClick={addNewTask}>
+        <i className="fa fa-solid fa-plus"></i>
+        </button>
+      </div>
+      <ul className="todoList">
+        {todolist.map((task, index) => (
+        
+          <li key={index}>
+            {task}
+            <button onClick={() => deleteTask(task)}><i className="fa-solid fa-trash"></i></button>
+          </li>
+       
+        ))}
+      </ul>
+      <div className="footer">
+        <span>You have {todolist.length} pending tasks</span>
+        <button onClick={() => setTodolist([])}>Clear All</button>
+      </div>
     </div>
   );
 }
